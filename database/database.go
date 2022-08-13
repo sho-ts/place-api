856496048f5ca81cc5e5608fc2d0a24051185a97
tagawa shouta <sho.webfe@gmail.com>
2022-08-14 04:43:15 +0900

    parseTimeをtrueに変更

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func Connect() {
 	var err error
 
-	dsn := os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_PASSWORD") + "@tcp(" + os.Getenv("MYSQL_HOST") + ")/" + os.Getenv("MYSQL_DBNAME")
+	dsn := os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_PASSWORD") + "@tcp(" + os.Getenv("MYSQL_HOST") + ")/" + os.Getenv("MYSQL_DBNAME") + "?parseTime=true"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
