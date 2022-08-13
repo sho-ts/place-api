c5dbb8a67ab65b4bcd8a2e9750983dbbb600bca1
tagawa shouta <sho.webfe@gmail.com>
2022-08-14 05:02:43 +0900

    idをcognitoのidにする

--- entity/users.go
+++ entity/users.go
@@ -2,9 +2,8 @@ package entity
 
 type User struct {
 	Entity
-	Id        string `gorm:"size:255;primary_key"`
-	AuthId    string
-	DisplayId string
-	Name      string
-	Avatar    string
+	Id        string `json:"-" gorm:"size:255;primary_key"`
+	DisplayId string `json:"userId"`
+	Name      string `json:"name"`
+	Avatar    string `json:"avatar"`
 }
--- service/user.go
+++ service/user.go
@@ -3,13 +3,11 @@ package service
 import (
 	"github.com/sho-ts/place-api/database"
 	"github.com/sho-ts/place-api/entity"
-	"github.com/sho-ts/place-api/util"
 )
 
 func CreateUser(authId string, displayId string, name string) entity.User {
 	user := entity.User{
-		Id:        util.GetULID(),
-		AuthId:    authId,
+		Id:        authId,
 		DisplayId: displayId,
 		Name:      name,
 	}
@@ -26,7 +24,7 @@ func CreateUser(authId string, displayId string, name string) entity.User {
 func GetMe(authId string) entity.User {
 	var user entity.User
 
-	database.DB.Where("auth_id = ?", authId).First(&user)
+	database.DB.Where("id = ?", authId).First(&user)
 
 	return user
 }
