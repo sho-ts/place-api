803abf8b9790cbb6bc7f1e355effdd5589719463
tagawa shouta <sho.webfe@gmail.com>
2022-08-13 23:11:10 +0900

    authIdをrequestBodyから取得する

--- controller/user.go
+++ controller/user.go
@@ -2,23 +2,20 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	jwtgo "github.com/golang-jwt/jwt"
 	"github.com/sho-ts/place-api/service"
-	"github.com/sho-ts/place-api/util"
 )
 
 func CreateUser(c *gin.Context) {
-	token := util.GetAuthResult(c)
-	clims := token.Claims.(jwtgo.MapClaims)
 
 	var requestBody struct {
+		AuthId    string `json:"authId"`
 		Name      string `json:"name"`
 		DisplayId string `json:"userId"`
 	}
 
 	c.ShouldBindJSON(&requestBody)
 
-	user := service.CreateUser(clims["sub"].(string), requestBody.DisplayId, requestBody.Name)
+	user := service.CreateUser(requestBody.AuthId, requestBody.DisplayId, requestBody.Name)
 
 	c.JSON(200, user)
 }
