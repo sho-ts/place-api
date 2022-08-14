9f1a91cbdfd6afce02238deaafc77df553777e49
tagawa shouta <sho.webfe@gmail.com>
2022-08-14 16:20:27 +0900

    変数名省略

--- route/router.go
+++ route/router.go
@@ -9,8 +9,8 @@ import (
 )
 
 func GetRouter() *gin.Engine {
-	router := gin.Default()
-	router.Use(middleware.GetCorsOption())
+	r := gin.Default()
+	r.Use(middleware.GetCorsOption())
 
 	mw, err := cognito.AuthJWTMiddleware(os.Getenv("AWS_COGNITO_ISS"), os.Getenv("AWS_COGNITO_USER_POOL_ID"), os.Getenv("AWS_COGNITO_REGION"))
 
@@ -18,22 +18,22 @@ func GetRouter() *gin.Engine {
 		panic("router Error")
 	}
 
-	router.GET("/api/v1/hello", controller.GetHello)
+	r.GET("/api/v1/hello", controller.GetHello)
 
-	router.GET("/api/v1/user", mw.MiddlewareFunc(), controller.GetMe)
-	router.POST("/api/v1/user", controller.CreateUser)
-	router.GET("/api/v1/user/:userId", controller.GetUser)
-	router.GET("/api/v1/user/:userId/posts", controller.GetUserPosts)
-	router.GET("/api/v1/user/duplicate/:userId", controller.CheckDuplicateUser)
+	r.GET("/api/v1/user", mw.MiddlewareFunc(), controller.GetMe)
+	r.POST("/api/v1/user", controller.CreateUser)
+	r.GET("/api/v1/user/:userId", controller.GetUser)
+	r.GET("/api/v1/user/:userId/posts", controller.GetUserPosts)
+	r.GET("/api/v1/user/duplicate/:userId", controller.CheckDuplicateUser)
 
-	router.GET("/api/v1/post", controller.GetPosts)
-	router.POST("/api/v1/post", mw.MiddlewareFunc(), controller.CreatePost)
-	router.GET("/api/v1/post/:postId", controller.GetPost)
-	router.GET("/api/v1/post/:postId/like/count", controller.GetLikeCount)
-	router.PUT("/api/v1/post/like", mw.MiddlewareFunc(), controller.Like)
+	r.GET("/api/v1/post", controller.GetPosts)
+	r.POST("/api/v1/post", mw.MiddlewareFunc(), controller.CreatePost)
+	r.GET("/api/v1/post/:postId", controller.GetPost)
+	r.GET("/api/v1/post/:postId/like/count", controller.GetLikeCount)
+	r.PUT("/api/v1/post/like", mw.MiddlewareFunc(), controller.Like)
 
-	router.GET("/api/v1/post/:postId/comment", controller.GetComments)
-	router.POST("/api/v1/post/comment", mw.MiddlewareFunc(), controller.CreateComment)
+	r.GET("/api/v1/post/:postId/comment", controller.GetComments)
+	r.POST("/api/v1/post/comment", mw.MiddlewareFunc(), controller.CreateComment)
 
-	return router
+	return r
 }
