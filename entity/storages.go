5b53776c1b4c47ae80b7fefa075af76311a24a91
tagawa shouta <sho.webfe@gmail.com>
2022-08-14 15:15:33 +0900

    jsonのkey名修正

--- entity/storages.go
+++ entity/storages.go
@@ -2,8 +2,8 @@ package entity
 
 type Storage struct {
 	Entity
-	Id     string `gorm:"size:255;primary_key"`
+	Id     string `gorm:"size:255;primary_key" json:"id"`
 	UserId string `json:"-"`
 	PostId string `json:"-"`
-	Url    string
+	Url    string `json:"url"`
 }
