75dc37bccdcc32807047e307d23fc4d21d643b43
tagawa shouta <sho.webfe@gmail.com>
2022-08-15 01:28:15 +0900

    ルート整理

---
+++ route/middleware/auth.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	cognito "github.com/akhettar/gin-jwt-cognito"
+	"os"
+)
+
+func GetAuthMiddleware() *cognito.AuthMiddleware {
+	mw, err := cognito.AuthJWTMiddleware(os.Getenv("AWS_COGNITO_ISS"), os.Getenv("AWS_COGNITO_USER_POOL_ID"), os.Getenv("AWS_COGNITO_REGION"))
+
+	if err != nil {
+		panic("router Error")
+	}
+
+  return mw
+}
--- route/router.go
+++ route/router.go
@@ -1,40 +1,35 @@
 package route
 
 import (
-	cognito "github.com/akhettar/gin-jwt-cognito"
 	"github.com/gin-gonic/gin"
 	"github.com/sho-ts/place-api/controller"
 	"github.com/sho-ts/place-api/route/middleware"
-	"os"
 )
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(middleware.GetCorsOption())
 
-	mw, err := cognito.AuthJWTMiddleware(os.Getenv("AWS_COGNITO_ISS"), os.Getenv("AWS_COGNITO_USER_POOL_ID"), os.Getenv("AWS_COGNITO_REGION"))
+	pr := r.Group("/api/v1")
+	ar := r.Group("/api/v1")
+	ar.Use(middleware.GetAuthMiddleware().MiddlewareFunc())
 
-	if err != nil {
-		panic("router Error")
-	}
+  // Public Routes
+	pr.GET("/hello", controller.GetHello)
+	pr.POST("/user", controller.CreateUser)
+	pr.GET("/user/:userId", controller.GetUser)
+	pr.GET("/user/:userId/posts", controller.GetUserPosts)
+	pr.GET("/user/duplicate/:userId", controller.CheckDuplicateUser)
+	pr.GET("/post", controller.GetPosts)
+	pr.GET("/post/:postId", controller.GetPost)
+	pr.GET("/post/:postId/like/count", controller.GetLikeCount)
+	pr.GET("/post/:postId/comment", controller.GetComments)
 
-	r.GET("/api/v1/hello", controller.GetHello)
-
-	r.GET("/api/v1/user", mw.MiddlewareFunc(), controller.GetMe)
-	r.POST("/api/v1/user", controller.CreateUser)
-	r.GET("/api/v1/user/:userId", controller.GetUser)
-	r.GET("/api/v1/user/:userId/posts", controller.GetUserPosts)
-	r.GET("/api/v1/user/duplicate/:userId", controller.CheckDuplicateUser)
-
-	r.GET("/api/v1/post", controller.GetPosts)
-	r.POST("/api/v1/post", mw.MiddlewareFunc(), controller.CreatePost)
-	r.GET("/api/v1/post/:postId", controller.GetPost)
-  
-	r.GET("/api/v1/post/:postId/like/count", controller.GetLikeCount)
-	r.PUT("/api/v1/post/like", mw.MiddlewareFunc(), controller.Like)
-
-	r.GET("/api/v1/post/:postId/comment", controller.GetComments)
-	r.POST("/api/v1/post/comment", mw.MiddlewareFunc(), controller.CreateComment)
+  // Auth Routes
+	ar.GET("/user", controller.GetMe)
+	ar.POST("/post", controller.CreatePost)
+	ar.PUT("/post/like", controller.Like)
+	ar.POST("/post/comment", controller.CreateComment)
 
 	return r
 }
