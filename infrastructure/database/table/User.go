c8c534c5c98a0ad844aa3a5722ce47e5a8fef122
tagawa shouta <sho.webfe@gmail.com>
2022-08-23 08:01:01 +0900

    構造体の記述を修正

--- infrastructure/database/table/User.go
+++ infrastructure/database/table/User.go
@@ -1,7 +1,7 @@
 package table
 
 type User struct {
-  Id        string `json:"-" gorm:"size:255;primary_key" gorm:"unique"`
+  Id        string `gorm:"size:255;primary_key;unique" json:"-"`
 	DisplayId string `json:"userId"`
 	Name      string `json:"name"`
 	Avatar    string `json:"avatar"`
