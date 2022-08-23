00966e134e53d8c3209d6fad37cbe13ab4a3b15d
tagawa shouta <sho.webfe@gmail.com>
2022-08-24 08:36:39 +0900

    CheckDuplicateを追加

--- domain/repository/IFollowRepository.go
+++ domain/repository/IFollowRepository.go
@@ -3,4 +3,5 @@ package repository
 type IFollowRepository interface {
 	Store(followUserId string, followerUserId string) error
 	Remove(followUserId string, followerUserId string) error
+	CheckDuplicate(postId string, userId string) (bool, error)
 }
